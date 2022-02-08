780222cc2f85d14166264c8283a235f69d2e74df
arenHome <aren.aren.97@mail.ru>
2022-02-08 19:22:03 +0300

    your message

--- functions/redirect.js
+++ functions/redirect.js
@@ -9,12 +9,13 @@ exports.handler = async event => {
     }
   } else {
     console.log('event', event)
-    // return {
-    //   statusCode: 301,
-    //   headers: {
-    //     'cache-control': 'public, max-age=0, must-revalidate',
-    //     location: 'http://netlify.asargsyan.ru/' + decodeURIComponent(event.queryStringParameters.url).split('/')[3] + '/'
-    //   }
-    // }
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        // location: 'http://netlify.asargsyan.ru/' + decodeURIComponent(event.queryStringParameters.url).split('/')[3] + '/'
+        location: 'http://netlify.asargsyan.ru?event=' + event
+      }
+    }
   }
 }
