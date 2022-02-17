772c3259228779e923cc13a77c299ffd5a027f63
arenHome <aren.aren.97@mail.ru>
2022-02-17 20:22:53 +0300

    your message

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,12 +1,3 @@
-const getServerSideProps = async () => {
-  // Fetch data from external API
-  const res = await fetch('http://netlify.asargsyan.ru/2022/02/08/test-1/?url=2022%2F02%2F08%2Ftest-1%2F')
-  const data = await res.json()
-
-  // Pass data to the page via props
-  return { props: { data } }
-}
-
 exports.handler = async event => {
   if (event.queryStringParameters.fbclid) {
     return {
