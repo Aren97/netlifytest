9e4df7fd039df98555fca58680e289dc683a1950
arenHome <aren.aren.97@mail.ru>
2022-02-17 20:32:57 +0300

    your message

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 $url = "https://sugareza.com/";
-$metas = get_meta_tags($url);
+$metas = { a: 1 };
 echo "<pre>"; print_r($metas); echo "</pre>";
 ?>
 </body>
