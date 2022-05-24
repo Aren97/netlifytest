21458b75b793ca8fb629028091aa41426c4fe7b0
arenHome <aren.aren.97@mail.ru>
2022-05-24 21:21:59 +0300

    your message

--- functions/redirect.js
+++ functions/redirect.js
@@ -14,23 +14,6 @@ exports.handler = async event => {
       }
     }
   } else {
-    request(event.queryStringParameters.url, {
-      timeout: 3000
-    }, (error, response, body) => {
-      if(!error) {
-        return {
-          statusCode: 200,
-          headers: {
-            'cache-control': 'no-cache',
-            'content-type': 'text/html; charset=utf-8'
-            // location: 'http://netlify.asargsyan.ru/' + decodeURIComponent(event.queryStringParameters.url).split('/')[3] + '/'
-            // location: 'http://netlify.asargsyan.ru?event=' + JSON.stringify(getServerAttr)
-          },
-          body
-        }
-      }
-    });
-
     return {
       statusCode: 200,
       headers: {
@@ -39,18 +22,7 @@ exports.handler = async event => {
         // location: 'http://netlify.asargsyan.ru/' + decodeURIComponent(event.queryStringParameters.url).split('/')[3] + '/'
         // location: 'http://netlify.asargsyan.ru?event=' + JSON.stringify(getServerAttr)
       },
-      body: '<html lang="en">\n' +
-        '<head>\n' +
-        '  <meta charset="UTF-8">\n' +
-        '  <meta name="viewport"\n' +
-        '        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">\n' +
-        '  <meta http-equiv="X-UA-Compatible" content="ie=edge">\n' +
-        '  <title>test etearaetaetea taet ae e</title>\n' +
-        '</head>\n' +
-        '<body>\n' +
-        '\n' +
-        '</body>\n' +
-        '</html>'
+      body: event.queryStringParameters.url
     }
   }
 }
