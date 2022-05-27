83bfc7c61e971e10eb4eb1f08e5a1e2a3eda467a
arenHome <aren.aren.97@mail.ru>
2022-05-27 15:15:56 +0300

    your message

--- functions/redirect.js
+++ functions/redirect.js
@@ -22,26 +22,15 @@ exports.handler = async event => {
       .then((response) => {
         return {
           statusCode: 200,
-          body: response.data,
+          // body: response.data,
+          body: decodeURIComponent(event.queryStringParameters.url.split('?')[0])
         };
       })
       .catch((error) => {
         console.log(error);
         return {
           statusCode: 500,
-          // body: JSON.stringify(error.message),
-          body: `<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>${decodeURIComponent(event.queryStringParameters.url.split('?')[0])}</title>
-</head>
-<body>
-
-</body>
-</html>`
+          body: JSON.stringify(error.message),
         };
       });
   }
