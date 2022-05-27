b1b197bfcd91efba6b69ab05ae3a1bf101e472ea
arenHome <aren.aren.97@mail.ru>
2022-05-27 16:35:13 +0300

    your message

--- functions/go.js
+++ functions/go.js
@@ -1,17 +1,19 @@
 const axios = require('axios');
 exports.handler = async event => {
+  const url = event.queryStringParameters.url
+
   if (event.queryStringParameters.fbclid) {
     return {
       statusCode: 301,
       headers: {
         'cache-control': 'no-cache',
-        location: decodeURIComponent(event.queryStringParameters.url)
+        location: decodeURIComponent(url.split('?')[0])
       }
     }
   } else {
     return axios({
       method: "get",
-      url: event.queryStringParameters.url,
+      url,
     })
       .then((response) => {
         return {
