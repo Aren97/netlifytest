aefe4bea5bb3cbe360a8eb0e5245ae84e36aaf2c
arenHome <aren.aren.97@mail.ru>
2022-05-27 15:51:30 +0300

    your message

--- functions/go.js
+++ functions/go.js
@@ -19,7 +19,20 @@ exports.handler = async event => {
       .then((response) => {
         return {
           statusCode: 200,
-          body: response.data
+          // body: response.data,
+          body: `<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>test etearaetaetea taet ae e</title>
+</head>
+<body>
+queryStringParameters.url - ${event.queryStringParameters.url} <br><br>
+url -${url}
+</body>
+</html>`
         };
       })
       .catch((error) => {
