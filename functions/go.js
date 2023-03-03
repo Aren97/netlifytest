ff40ee784477bde204adac036cf452852a5f4c24
Aren97 <aren.aren.97@mail.ru>
2023-03-03 20:55:07 +0300

    Update go.js
    change domain

--- functions/go.js
+++ functions/go.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 exports.handler = async event => {
-  const url = `https://mondeanimalinteressant.com${event.queryStringParameters.url}`
+  const url = `https://amazingstories.fun/${event.queryStringParameters.url}`
 
   const isFromFb = event.multiValueHeaders.Referer && event.multiValueHeaders.Referer.some(item => item.includes('facebook'))
 
